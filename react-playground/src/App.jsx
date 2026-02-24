90355918c49c065297f4e7773173792435c41b48
Rejoan Siddiky <siddikyrejoan@gmail.com>
2026-02-25 03:45:21 +0600

    Arranged a playground for react with component base practices along with courses

---
+++ react-playground/src/App.jsx
@@ -0,0 +1,14 @@
+// Import any practices from src/practices here 
+import MapPractice from "./practices/MapPractice"; // Map Iterating Practice
+
+
+
+
+// Current Component (Practice File), Change it when needed.
+const CURRENT_COMPONENT = MapPractice;
+
+// export the App component that render each practices
+export default function App() {
+  /*Return CURRENT_COMPONENT  */
+  return <CURRENT_COMPONENT />;
+}
